fbe8ebd327bdea63ef71c934f4975f4a310cd077
unknown <taphu0812@gmail.com>
2022-06-28 22:51:43 +0700

    bai2

--- BT5_B2.js
+++ BT5_B2.js
@@ -1,6 +1,20 @@
-let usd = money * 23000;
-let vnd = mney / 23000;
-function hamchuyendoitiente(){
-        let
 
+function hamchuyendoitiente(){
+       let ketqua;
+       let nhaptien= + document.getElementById('nhaptien').value;
+       let nhap = document.getElementById('chuyendoi').value;
+       console.log('chuyen',nhap);
+       let xuat = document.getElementById('chuyendoi2').value;
+       console.log('xuat',xuat);
+       if (nhap===xuat){
+              ketqua=nhaptien;
+       }
+       else {
+              if (nhap === 'VND') {
+                     ketqua = nhaptien / 23000;
+              } else {
+                     ketqua= nhaptien*23000;
+              }
+       }
+     document.getElementById('ketqua').innerHTML=ketqua + ' ' + xuat;
 }
